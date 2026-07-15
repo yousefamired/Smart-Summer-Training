52277de2ee38676f213d765a5369d06b08c47285
Arwa Elkersh <arwaelkersh@Arwas-MacBook-Pro.local>
2026-07-15 10:20:35 +0300

    Book Class

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,7 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 	public static void main(String[] args) {
-
+		Book b1=new PhysicalBook("Dracula","Bram Stoker",1,"Horror",300,10);
+		Book b2=new Ebook("Beloved","Toni Morrison",2,"Fiction",300);
+		Book b3=new PhysicalBook("Lolita", "Vladimir Nabokov",3,"Fiction",400,20);
+		Book b4=new Ebook("Dune","Frank Herbert",4,"Fantasy",400);
+		System.out.println(b1.getBookHashMap());
 
 		}
 
